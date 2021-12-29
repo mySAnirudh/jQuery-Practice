8ca91659d66ff00cccc6c01efa659079fe62803c
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2021-12-29 10:46:56 +0530

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,16 @@
+var a =10;
+
+var b =20;
+// // var linebreak = "<br/>";
+// document.write("(a==b)=>");
+
+// result=(a==b);
+// document.write(result);
+// // document.write(linebreak);
+
+// result=(a<b);
+// document.write("(a<b)=>");
+
+document.write(a<b);
+document.write(a>=b);
+document.write(a<=b)
